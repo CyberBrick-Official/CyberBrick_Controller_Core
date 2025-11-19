afad7203d0addab7d69788686bf35de5faa24ad8
jiaxiong.deng <jiaxiong.deng@bambulab.com>
2025-11-19 17:57:23 +0800

    MOD: Update version to 01.00.00.21

--- src/app_rc/boot.py
+++ src/app_rc/boot.py
@@ -10,7 +10,7 @@ import bbl_product
 import sys
 
 _PRODUCT_NAME = "RC"
-_PRODUCT_VERSION = "01.00.00.20"
+_PRODUCT_VERSION = "01.00.00.21"
 
 bbl_product.set_app_name(_PRODUCT_NAME)
 bbl_product.set_app_version(_PRODUCT_VERSION)
